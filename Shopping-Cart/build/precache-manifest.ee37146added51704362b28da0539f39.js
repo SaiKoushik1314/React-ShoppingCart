ccf52a1936e98911fd0fc776699a0b5dc5efd2d8
SaiKoushik1314 <sai.koushik1314@gmail.com>
2020-07-26 19:31:39 -0400

    first commit.

---
+++ Shopping-Cart/build/precache-manifest.ee37146added51704362b28da0539f39.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ed87ff92351141e29cb",
+    "url": "/React-ShoppingCart/static/css/main.7bc35809.chunk.css"
+  },
+  {
+    "revision": "8ed87ff92351141e29cb",
+    "url": "/React-ShoppingCart/static/js/main.8ed87ff9.chunk.js"
+  },
+  {
+    "revision": "99d7c59f45359249397b",
+    "url": "/React-ShoppingCart/static/js/1.99d7c59f.chunk.js"
+  },
+  {
+    "revision": "7e658920b96e9cdac8fa",
+    "url": "/React-ShoppingCart/static/js/runtime~main.7e658920.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/React-ShoppingCart/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/React-ShoppingCart/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/React-ShoppingCart/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/React-ShoppingCart/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/React-ShoppingCart/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "a8ff83a3afa17ca546ac5bd632ebb198",
+    "url": "/React-ShoppingCart/index.html"
+  }
+];
